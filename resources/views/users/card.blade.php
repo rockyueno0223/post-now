be2e7e43442cbda35d20daa9ec3a18d60d6aac3f
Hiroki Ueno <rockyueno@gmail.com>
2024-06-16 00:24:54 -0700

    Update Card style

--- resources/views/users/card.blade.php
+++ resources/views/users/card.blade.php
@@ -2,7 +2,7 @@
     <div class="card-header">
         <h3 class="card-title">{{ $user->name }}</h3>
     </div>
-    <div class="card-body">
+    <div class="card-body p-5">
         <img class="rounded img-fluid" src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Mario" alt="">
     </div>
 </div>
