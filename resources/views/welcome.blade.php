c53ab1ccb05200f0113d6353a77580e126ecb3bd
Hiroki Ueno <rockyueno@gmail.com>
2024-08-30 16:15:26 -0700

    updated welcome page style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="d-flex justify-content-center">
-            <div style="height: 360px" class="w-75 bg-secondary rounded text-center text-light d-flex flex-column justify-content-center align-items-center">
-                <h1 class="mb-3">Post Now!</h1>
-                <a href="{{ route('register') }}" class="btn btn-lg btn-dark">Register now!</a>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-7 d-flex justify-content-center align-items-center">
+                <img src="{{ asset('storage/image/main-visual.png') }}" alt="Post Now!" class="w-100">
+            </div>
+            <div style="height: 280px" class="col-md-5 text-center d-flex flex-column justify-content-center align-items-center">
+                <h1 style="font-family: 'Barlow Condensed'" class="">Post Now!</h1>
+                <p class="fs-1 fw-medium mb-4" style="font-family: 'Barlow Condensed'">Share Now!</p>
+                <a href="{{ route('register') }}" class="btn btn-lg btn-dark">Start Now!</a>
             </div>
         </div>
     </div>
